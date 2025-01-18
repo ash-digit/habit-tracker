1bee7f5a6112f695f2ce30064cd43d0913371581
Ash digit <ashdigit44@gmail.com>
2025-01-18 20:42:05 +0000

    doing some react training

--- habit-tracker/src/App.jsx
+++ habit-tracker/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Habit } from './Habit'
+import { Colours } from './colours'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +9,11 @@ function App() {
   return (
     <>
     <section class="flex items-center justify-center h-screen bg-green-400">
-      <section class="border-2 rounded border-black bg-blue-500 text-cyan-100 p-9">  
-        <Habit  props habitName = "Reading" streak = "2"/>
+      <section class="">  
+        {/* <Habit  props habitName = "Reading" streak = "2"/> */}
+        <Colours />
       </section>
+      
     </section>
     
     </>
---
+++ habit-tracker/src/colours.jsx
@@ -0,0 +1,10 @@
+export const Colours = () =>{
+    return(
+        <>
+            <div>Blue</div>
+            <div>Green</div>
+            <div>Yellow</div>
+            <div>Orange</div>
+        </>
+    )
+}
