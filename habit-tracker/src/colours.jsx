c1803647063e09ddcf0daf16098b5c79d950ccf2
Ash digit <ashdigit44@gmail.com>
2025-01-19 23:55:31 +0000

    added a colour class because of Tailwind’s default purge

--- habit-tracker/src/colours.jsx
+++ habit-tracker/src/colours.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react"
 
+const colorToClass = {
+    blue:  "bg-blue-200",
+    green: "bg-green-200",
+    red:   "bg-red-200",
+    amber: "bg-amber-200"
+  };
+
 export const Colours = () =>{
     const [colour, setColour] = useState("blue")
-    const [cClass, setCClass] = useState("")
+    const [cClass, setCClass] = useState("border-2 w-24 h-20 bg-lime-200")
     useEffect(()=>{
 
     },[colour,cClass])
@@ -18,7 +25,7 @@ export const Colours = () =>{
             <div onClick={()=>{handleColourChange('blue')}}>Blue</div>
             <div onClick={()=>{handleColourChange('green')}}>Green</div>
             <div onClick={()=>{handleColourChange('red')}}>red</div>
-            <div onClick={()=>{handleColourChange('orange')}}>Orange</div>
+            <div onClick={()=>{handleColourChange('amber')}}>Amber</div>
             <div>The chosen colour: {colour}</div>
             <div className={cClass}></div>
         </>
